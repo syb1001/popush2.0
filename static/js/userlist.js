5d30e4dca5c15ab7977f522b3768b81d2a0a0fb0
lv wanqi <lvwanqi11@gmail.com>
2013-10-15 19:30:40 +0800

    lvwanqi: userlist.js with explanation

--- static/js/userlist.js
+++ static/js/userlist.js
@@ -1,19 +1,27 @@
+//当前文件的所有共享用户的列表
 var allUserLists = [];
 
+//共享文件时的用户相关操作
 function userList(div) {
 
+	//obj为当前页面中的div元素（应该就是显示添加用户的那个层）
 	var obj = $(div);
 	
+	//elements是用户信息
 	var elements = [];
 	
+	//n是当前用户列表的长度
 	var n = allUserLists.length;
 	
+	//标记被选中的用户
 	var selected;
 	
+	//r是一个当前文件所共享的用户对象
 	r = {
 		
 		elements: elements,
 		
+		//删除用户
 		clear: function() {
 			obj.html('');
 			elements = [];
@@ -21,6 +29,7 @@ function userList(div) {
 			selected = null;
 		},
 		
+		//添加用户
 		add: function(user) {
 			var i = elements.length;
 			obj.append(
@@ -30,16 +39,19 @@ function userList(div) {
 			return elements.push(user);
 		},
 		
+		//获取当前被选择的用户
 		getselection: function() {
 			return selected;
 		},
 		
+		//当用户被选择时发生的事情；将该用户的class改成active，将selected改为该用户
 		onselect: function(i) {
 			obj.find('li').removeClass('active');
 			obj.find('li:eq('+i+')').addClass('active');
 			selected = elements[i];
 		},
 		
+		//从服务器读取当前文件的共享用户列表，显示到当前div里
 		fromusers: function(users) {
 			this.clear();
 			users.sort(function(a,b) {
@@ -58,6 +70,7 @@ function userList(div) {
 
 }
 
+//在代码编辑窗口以及个人页面的文档共享用户操作
 function userListAvatar(div) {
 
 	var obj = $(div);
@@ -76,12 +89,14 @@ function userListAvatar(div) {
 			this.elements = elements;
 		},
 		
+		//在分享中添加用户
 		add: function(user, owner) {
 			var userobj = $(
 				'<img id="avatar' + n + '-' + user.name + '" src="' + user.avatar + '" width="40" height="40"' +
 				' class="pull-left online shared-character user-' + user.name + '" />'
 				);
 			obj.append(userobj);
+			//悬浮时弹出每个用户的名字和头像
 			$('#avatar' + n + '-' + user.name).popover({
 				html: true,
 				content: '<img class="pull-left popover-character user-' + user.name + '" src="' + user.avatar + '" width="48" height="48" />' +
@@ -97,12 +112,14 @@ function userListAvatar(div) {
 			elements[user.name] = user;
 		},
 		
+		//从分享中移除用户
 		remove: function(username) {
 			$('#avatar' + n + '-' + username).remove();
 			if(elements[username])
 				delete elements[username];
 		},
 		
+		//读取当前文档的所有共享用户
 		fromdoc: function(doc) {
 			this.clear();
 			doc.members.sort(function(a,b) {
@@ -115,6 +132,7 @@ function userListAvatar(div) {
 			}
 		},
 		
+		//更新悬浮弹出
 		refreshpopover: function(user) {
 			$('#avatar' + n + '-' + user.name).popover('destroy');
 			$('#avatar' + n + '-' + user.name).popover({
@@ -126,6 +144,7 @@ function userListAvatar(div) {
 			});
 		},
 		
+		//当一个用户进入了共享的文件编辑，将其设置为online，在别人处看到的是在线状态
 		setonline: function(username, online) {
 			if(online)
 				$('#avatar' + n + '-' + username).addClass('online');
@@ -134,6 +153,7 @@ function userListAvatar(div) {
 			elements[username].online = online;
 		},
 		
+		//当用户退出编辑，取消online属性
 		setalloffline: function() {
 			for(var i in elements) {
 				var user = elements[i];
@@ -142,6 +162,7 @@ function userListAvatar(div) {
 			}
 		},
 		
+		//对用户排序
 		sort: function() {
 			var arr = [];
 			for(var i in elements) {
